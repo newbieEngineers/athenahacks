4685ff090e90c2defbb45593efac59b6586d56f0
Jessie Tsai <jessiets@usc.edu>
2021-03-07 03:56:18 -0800

    setup for deployment

--- usc_sos/App.js
+++ usc_sos/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, Linking, Platform } from 'react-native';
+import { StyleSheet, Text, View, Button, Linking, TouchableOpacity, Platform } from 'react-native';
 
 export default class App extends Component {
   constructor(){
@@ -17,15 +17,25 @@ export default class App extends Component {
     Linking.openURL(phoneNumber);
  };
 
+  // TouchableOpacity.defaultProps = { activeOpacity: 0.8 };
+
+  // const AppButton = ({ onPress, title}) => (
+  //   <TouchableOpacity onPress={()=>{this.dialCall(+12133751230)}} style={styles.appButtonContainer}>
+  //     <Text style={styles.appButtonText}>{title}</Text>
+  //   </TouchableOpacity>
+  // );
+
   render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Welcome to USC_SOS! 
-           <br>The place that bundles up emergency contacts to keep you safe :)</br></Text>
-        <View style={styles.buttonContainer}>
-          <Button title="Call USC_SOS" onPress={()=>{this.dialCall(+12133751230)}}></Button>
+        <Text style={styles.title}>Welcome to {"\n"} USC SOS! {"\n"}</Text>
+        <Text style={styles.bodyText}>The place that bundles up emergency contacts to{"\n"} keep you safe :)</Text>
+        <View style={{alignItems:'center', justifyContent:'center'}}>
+        <Button title="Call USC_SOS" size="lg" color='#bd2031' textAlign='center' onPress={()=>{this.dialCall(+12133751230)}}>
+          <Text style={{marginLeft:50}}>Call USC_SOS</Text>
+        </Button>
+          <Text style={{color:'white'}}>{this.props.Name}</Text>
         </View>
-        <StatusBar style="auto" />
       </View>
     );
   };
@@ -45,12 +55,32 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20
   },
+  bodyText:{
+    fontSize: 20,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginBottom: 30
+  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    textAlign: 'center',
     width: '80%',
     paddingTop: 20, 
     paddingBottom: 20, 
-    borderColor: "#033dfc"
+    borderColor: "#bd2031"
   }
+  // appButtonContainer: {
+  //   elevation: 8,
+  //   backgroundColor: "#bd2031",
+  //   borderRadius: 10,
+  //   paddingVertical: 10,
+  //   paddingHorizontal: 12
+  // },
+  // appButtonText: {
+  //   fontSize: 20,
+  //   color: "white",
+  //   fontWeight: "bold",
+  //   alignSelf: "center",
+  // }
 });
